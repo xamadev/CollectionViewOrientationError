34866be1532bedb781ddc17c4296340983bd7d26
xamadev <46600429+xamadev@users.noreply.github.com>
2019-03-12 15:10:03 +0100

    Add files via upload

---
+++ CollectionViewOrientationError/CollectionViewOrientationError/CollectionViewOrientationError/Views/CustomLayouts/SqaureLayout/SquareLayout.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace C4S.MobileApp.Views.CustomLayouts
+{
+    public class SquareLayout : Layout<View>
+    {
+        //Dictionary<Size, LayoutData> layoutDataCache = new Dictionary<Size, LayoutData>();
+
+        protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
+        {
+            //Keine Views -> kein Platz benötigt
+            if (Children.Count == 0)
+            {
+                return new SizeRequest();
+            }
+
+            Size size;
+
+            if (Double.IsPositiveInfinity(heightConstraint)
+            ) //Display des Geräts in Portrait -> verfügabare Breite ist relevant
+            {
+                size = new Size(widthConstraint, widthConstraint);
+            }
+            else //Display des Geräts in Landscape -> verfügabare Höhe ist relevant
+            {
+                size = new Size(heightConstraint, heightConstraint);
+            }
+
+            Debug.WriteLine($"OnMeasure: widthConstraint {widthConstraint}, heightConstraint {heightConstraint}, size " + size);
+
+            return new SizeRequest(size);
+        }
+
+        protected override void LayoutChildren(double x, double y, double width, double height)
+        {
+            //1.Child positionieren
+            LayoutChildIntoBoundingRegion(Children.FirstOrDefault(),
+                new Rectangle(new Point(x, y), new Size(width, width)));
+
+            Debug.WriteLine($"LayoutChildren: x {x}, y {y}, width {width}, height {height}");
+        }
+    }
+}
---
+++ CollectionViewOrientationError/CollectionViewOrientationError/CollectionViewOrientationError/Views/CustomsControls/CollectionViewWithOrientation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace C4S.MobileApp.Views.CustomsControls
+{
+    class CollectionViewWithOrientation : CollectionView
+    {
+        private GridItemsLayout GridItemsLayout = new GridItemsLayout(ItemsLayoutOrientation.Vertical);
+
+        public CollectionViewWithOrientation()
+        {
+            // Subscribe to changes of screen metrics
+            DeviceDisplay.MainDisplayInfoChanged += OnMainDisplayInfoChanged;
+
+            //Configure layout initial 
+            ItemsLayout = GridItemsLayout;
+            ConfigureLayout(DeviceDisplay.MainDisplayInfo.Orientation);
+        }
+        void OnMainDisplayInfoChanged(object sender, DisplayInfoChangedEventArgs e)
+        {
+            // Process changes
+            ConfigureLayout(e.DisplayInfo.Orientation);
+        }
+
+        void ConfigureLayout(DisplayOrientation displayOrientation)
+        {
+            GridItemsLayout.Span = displayOrientation == DisplayOrientation.Portrait ? 2 : 4;
+        }
+    }
+}
